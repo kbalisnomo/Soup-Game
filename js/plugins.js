6ad19e49cf9b6e660416bc8303ded03d5f0e8a9c
Quinn Pham <quinnlp@gmail.com>
2019-10-05 22:37:21 -0600

    Movement/Hitbox/Screen Scrolling
    - Disabled movement via arrow keys and mouse clicking
    - Added Hitbox Pluggin
    Comment tag in this format:
    <Hitbox Up: x>
    - added fire, click to scroll screen

--- js/plugins.js
+++ js/plugins.js
@@ -10,5 +10,6 @@ var $plugins =
 {"name":"YEP_EventClickTrigger","status":true,"description":"v1.00 Lets the player click on designated notetagged events to\r\ntrigger them from afar!","parameters":{}},
 {"name":"QPlus","status":true,"description":"<QPlus> (Should go above all Q Plugins)\nSome small changes to MV for easier plugin development.","parameters":{"Quick Test":"true","Default Enabled Switches":"[]","Ignore Mouse when inactive":"false"}},
 {"name":"YEP_RegionRestrictions","status":true,"description":"v1.04 Use regions to block out Events and/or the player from\nbeing able to venture into those spots.","parameters":{"Player Restrict":"0","Event Restrict":"0","All Restrict":"2","Player Allow":"0","Event Allow":"0","All Allow":"1"}},
-{"name":"YEP_MoveRouteCore","status":true,"description":"v1.04 Expand the number of movement options for the move\nroute events using this plugin.","parameters":{}}
+{"name":"YEP_MoveRouteCore","status":true,"description":"v1.04 Expand the number of movement options for the move\nroute events using this plugin.","parameters":{}},
+{"name":"YEP_EventHitboxResize","status":true,"description":"v1.00 Allows you to make larger event hitboxes, making the event\nable to be triggered from multiple tiles.","parameters":{}}
 ];
